ea6e2f5b83368cd3cf2f60f0a59867ee3744dde9
Serena-univ <luzvirtudk@gmail.com>
2025-07-28 18:30:02 +0900

    Update gas-proxy.js

--- netlify/functions/gas-proxy.js
+++ netlify/functions/gas-proxy.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 exports.handler = async function(event) {
   const params = event.queryStringParameters;
 
-  const url = new URL("https://script.google.com/macros/s/AKfycbxkhDUi1lxRkGHFxA7JVCWKlhUvIWbAD8xhxpEbN1ZCFN5b7PIIQIFZHbjRMo2OCMlj/exec");
+  const url = new URL("https://script.google.com/macros/s/AKfycbzKtNHiLbI7JYOBXH5U_JE0-K97e6K2fCw4B0MVkMjeDq2zmoORlMw9l8QsL_LieYs/exec");
   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
   try {
